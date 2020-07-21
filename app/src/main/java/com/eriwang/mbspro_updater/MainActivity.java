44b37d946ee240769106ab4706cc71d9e9c14dd2
Eric Wang <johnericcw@gmail.com>
2020-07-21 13:30:36 -0400

    Initial commit from Android studio

---
+++ app/src/main/java/com/eriwang/mbspro_updater/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eriwang.mbspro_updater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
