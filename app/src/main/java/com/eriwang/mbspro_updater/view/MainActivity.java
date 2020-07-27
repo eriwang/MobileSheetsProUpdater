8e6f101f4fbd376c268ae4b4cca496beda4fc43b
Eric Wang <johnericcw@gmail.com>
2020-07-27 19:31:55 -0400

    Attempt to hack in background service code. Seems to be stopping when app is closed

--- app/src/main/java/com/eriwang/mbspro_updater/view/MainActivity.java
+++ app/src/main/java/com/eriwang/mbspro_updater/view/MainActivity.java
@@ -3,14 +3,21 @@ package com.eriwang.mbspro_updater.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.eriwang.mbspro_updater.R;
 import com.eriwang.mbspro_updater.drive.DriveWrapper;
 import com.eriwang.mbspro_updater.sync.SongSyncManager;
+import com.eriwang.mbspro_updater.sync.SongSyncService;
 import com.eriwang.mbspro_updater.utils.FunctionWrapper;
 import com.eriwang.mbspro_updater.utils.ProdAssert;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -31,7 +38,10 @@ public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "Main";
     private static final int REQ_CODE_SIGN_IN = 1;
-    private static final int REQ_CODE_SYNC_TEST = 2;
+    private static final int REQ_CODE_FORCE_SYNC = 2;
+    private static final int REQ_CODE_START_BG_SYNC = 3;
+    private static final int REQ_CODE_BG_SYNC_SERVICE = 4;
+
 
     private static final String TEST_FOLDER_ROOT_ID = "11HTp4Y8liv9Oc0Sof0bxvlsGSLmQAvl4";
 
@@ -49,7 +59,8 @@ public class MainActivity extends AppCompatActivity
         mDrive = new DriveWrapper();
         mSongSyncManager = new SongSyncManager(mDrive, getApplicationContext());
 
-        findViewById(R.id.sync_test).setOnClickListener(view -> {
+        // TODO: rethink what's a user-friendly and more robust way to do this (e.g. what to save)
+        findViewById(R.id.force_sync_now).setOnClickListener(view -> {
             // TODO: should explicitly tell user what they should be selecting. also sanity check after that the db
             //       is there
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
@@ -57,8 +68,27 @@ public class MainActivity extends AppCompatActivity
                     .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
             // TODO: specify initial URI?
-            startActivityForResult(intent, REQ_CODE_SYNC_TEST);
+            startActivityForResult(intent, REQ_CODE_FORCE_SYNC);
         });
+        findViewById(R.id.schedule_sync_in_background).setOnClickListener(view -> {
+            // TODO: should explicitly tell user what they should be selecting. also sanity check after that the db
+            //       is there
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+            // TODO: specify initial URI?
+            startActivityForResult(intent, REQ_CODE_START_BG_SYNC);
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel("channel", "channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
 
         // TODO: should be more user friendly in the future
         requestSignIn();
@@ -73,8 +103,12 @@ public class MainActivity extends AppCompatActivity
             handleSignInResult(resultCode, resultData);
             break;
 
-        case REQ_CODE_SYNC_TEST:
+        case REQ_CODE_FORCE_SYNC:
             handleSyncTest(resultCode, resultData);
+            break;
+
+        case REQ_CODE_START_BG_SYNC:
+            handleStartBackgroundSync(resultCode, resultData);
             break;
 
         default:
@@ -132,6 +166,33 @@ public class MainActivity extends AppCompatActivity
 
         taskExecuteVoid(() -> mSongSyncManager.syncMbsProWithDrive(TEST_FOLDER_ROOT_ID, saveLocationUri))
                 .addOnFailureListener(exception -> Log.e(TAG, "Sync failed", exception));
+    }
+
+    private void handleStartBackgroundSync(int resultCode, Intent result)
+    {
+        // TODO: actual error handling
+        if (resultCode != Activity.RESULT_OK || result == null)
+        {
+            return;
+        }
+
+        final Uri saveLocationUri = result.getData();
+        ProdAssert.notNull(saveLocationUri);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        ProdAssert.notNull(alarmManager);
+
+        Intent intent = new Intent(this, SongSyncService.class)
+                .putExtra("saveLocationUri", saveLocationUri)
+                .putExtra("driveDirectoryId", TEST_FOLDER_ROOT_ID);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQ_CODE_BG_SYNC_SERVICE, intent, 0);
+        if (pendingIntent != null)
+        {
+            alarmManager.cancel(pendingIntent);
+        }
+
+        // TODO: inexact
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, 1000, 1000, pendingIntent);
     }
 
     private <T> Task<T> taskExecute(Callable<T> c)
