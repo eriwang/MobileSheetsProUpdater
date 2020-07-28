d8ed3e58952392df335cc5b0e89d21642aaeb5e0
Eric Wang <johnericcw@gmail.com>
2020-07-28 18:51:27 -0400

    SongSyncJobService now actually running the conversion job, called by both the force sync button and the bg sync button

--- app/src/main/java/com/eriwang/mbspro_updater/sync/SongSyncJobService.java
+++ app/src/main/java/com/eriwang/mbspro_updater/sync/SongSyncJobService.java
@@ -2,35 +2,69 @@ package com.eriwang.mbspro_updater.sync;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.net.Uri;
 import android.util.Log;
 
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import com.eriwang.mbspro_updater.drive.DriveWrapper;
+import com.eriwang.mbspro_updater.utils.ProdAssert;
+import com.eriwang.mbspro_updater.utils.TaskUtils;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.services.drive.DriveScopes;
 
-import com.eriwang.mbspro_updater.R;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class SongSyncJobService extends JobService
 {
-    private static int notifCount = 0;
+    public static final int JOB_ID = 1;
+    public static final String MBS_PRO_DATA_DIR = "MBS_PRO_DATA_DIR";
+    public static final String DRIVE_FOLDER_ID = "DRIVE_FOLDER_ID";
+
+    private static String TAG = "SongSyncJobService";
+
+    private Executor mExecutor;
+    private DriveWrapper mDrive;
+    private SongSyncManager mSongSyncManager;
+
+    @Override
+    public void onCreate()
+    {
+        mExecutor = Executors.newSingleThreadExecutor();
+        mDrive = new DriveWrapper();
+        mSongSyncManager = new SongSyncManager(mDrive, this);
+    }
 
     @Override
     public boolean onStartJob(JobParameters jobParameters)
     {
-        // TODO: should the sync run in the background?
-        Log.d("Receiver", "hello");
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel")
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                .setContentTitle("Hello")
-                .setContentText("Hello")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(notifCount++, builder.build());
-        return false;
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        ProdAssert.notNull(account);  // TODO: better handling of auth with the job. Maybe notification and cancel job
+
+        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                this, Collections.singletonList(DriveScopes.DRIVE))
+                .setSelectedAccount(account.getAccount());
+        mDrive.setCredentialAndInitialize(credential);
+
+        String mbsProDataDir = jobParameters.getExtras().getString(MBS_PRO_DATA_DIR);
+        String driveFolderId = jobParameters.getExtras().getString(DRIVE_FOLDER_ID);
+
+        Log.d(TAG, "Running job");
+        TaskUtils.execute(mExecutor, () -> {
+            mSongSyncManager.syncMbsProWithDrive(driveFolderId, Uri.parse(mbsProDataDir));
+            Log.d(TAG, "Job complete");
+            jobFinished(jobParameters, false);
+        });
+
+        return true;  // job still running
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters)
     {
-        return false;
+        // No-op here for now for simplicity.
+        return false;  // Don't reschedule
     }
 }
--- app/src/main/java/com/eriwang/mbspro_updater/view/MainActivity.java
+++ app/src/main/java/com/eriwang/mbspro_updater/view/MainActivity.java
@@ -13,25 +13,21 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 
 import com.eriwang.mbspro_updater.R;
-import com.eriwang.mbspro_updater.drive.DriveWrapper;
 import com.eriwang.mbspro_updater.sync.SongSyncJobService;
-import com.eriwang.mbspro_updater.sync.SongSyncManager;
-import com.eriwang.mbspro_updater.utils.FunctionWrapper;
 import com.eriwang.mbspro_updater.utils.ProdAssert;
+import com.eriwang.mbspro_updater.utils.TaskUtils;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.services.drive.DriveScopes;
 
 import java.util.Collections;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -42,8 +38,6 @@ public class MainActivity extends AppCompatActivity
     private static final int REQ_CODE_FORCE_SYNC = 2;
     private static final int REQ_CODE_START_BG_SYNC = 3;
 
-    private static final int JOB_ID_BG_SYNC = 1;
-
     // Note that Android clamps the interval to 15 minutes (possibly less on some versions of Android), and forces a
     // post-job "flex" interval of 5 minutes.
     private static final int BG_SYNC_INTERVAL_MILLIS = 5000;
@@ -51,8 +45,6 @@ public class MainActivity extends AppCompatActivity
     private static final String TEST_FOLDER_ROOT_ID = "11HTp4Y8liv9Oc0Sof0bxvlsGSLmQAvl4";
 
     private Executor mExecutor;
-    private DriveWrapper mDrive;
-    private SongSyncManager mSongSyncManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -61,33 +53,16 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mExecutor = Executors.newSingleThreadExecutor();
-        mDrive = new DriveWrapper();
-        mSongSyncManager = new SongSyncManager(mDrive, getApplicationContext());
 
-        // TODO: rethink what's a user-friendly and more robust way to do this (e.g. what to save)
-        findViewById(R.id.force_sync_now).setOnClickListener(view -> {
-            // TODO: should explicitly tell user what they should be selecting. also sanity check after that the db
-            //       is there
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        findViewById(R.id.force_sync_now).setOnClickListener(view ->
+            startPickerActivityWithRequestCode(REQ_CODE_FORCE_SYNC));
 
-            // TODO: specify initial URI?
-            startActivityForResult(intent, REQ_CODE_FORCE_SYNC);
-        });
-        findViewById(R.id.schedule_sync_in_background).setOnClickListener(view -> {
-            // TODO: should explicitly tell user what they should be selecting. also sanity check after that the db
-            //       is there
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        findViewById(R.id.schedule_sync_in_background).setOnClickListener(view ->
+            startPickerActivityWithRequestCode(REQ_CODE_START_BG_SYNC));
 
-            // TODO: specify initial URI?
-            startActivityForResult(intent, REQ_CODE_START_BG_SYNC);
-        });
         findViewById(R.id.stop_sync).setOnClickListener(view -> {
             JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-            jobScheduler.cancel(JOB_ID_BG_SYNC);
+            jobScheduler.cancel(SongSyncJobService.JOB_ID);
         });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
@@ -112,7 +87,7 @@ public class MainActivity extends AppCompatActivity
             break;
 
         case REQ_CODE_FORCE_SYNC:
-            handleSyncTest(resultCode, resultData);
+            handleForceSync(resultCode, resultData);
             break;
 
         case REQ_CODE_START_BG_SYNC:
@@ -140,6 +115,19 @@ public class MainActivity extends AppCompatActivity
         startActivityForResult(client.getSignInIntent(), REQ_CODE_SIGN_IN);
     }
 
+    private void startPickerActivityWithRequestCode(int requestCode)
+    {
+        // TODO: rethink what's a user-friendly and more robust way to do this (e.g. what to save)
+        // TODO: maybe explicitly tell user what they should be selecting. also sanity check after that the db
+        //       is there
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        // TODO: specify initial URI?
+        startActivityForResult(intent, requestCode);
+    }
+
     private void handleSignInResult(int resultCode, Intent result)
     {
         // TODO: actual error handling
@@ -149,19 +137,18 @@ public class MainActivity extends AppCompatActivity
         }
 
         GoogleSignIn.getSignedInAccountFromIntent(result)
-                .onSuccessTask(googleSignInAccount -> taskExecuteVoid(() -> {
+                .onSuccessTask(googleSignInAccount -> TaskUtils.execute(mExecutor, () -> {
                     ProdAssert.notNull(googleSignInAccount);
                     Log.d(TAG, "Signed in as " + googleSignInAccount.getEmail());
 
                     GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
                             this, Collections.singletonList(DriveScopes.DRIVE));
                     credential.setSelectedAccount(googleSignInAccount.getAccount());
-                    mDrive.setCredentialAndInitialize(credential);
                 }))
                 .addOnFailureListener(exception -> Log.e(TAG, "Unable to sign in.", exception));
     }
 
-    private void handleSyncTest(int resultCode, Intent result)
+    private void handleForceSync(int resultCode, Intent result)
     {
         // TODO: actual error handling
         if (resultCode != Activity.RESULT_OK || result == null)
@@ -169,11 +156,7 @@ public class MainActivity extends AppCompatActivity
             return;
         }
 
-        final Uri saveLocationUri = result.getData();
-        ProdAssert.notNull(saveLocationUri);
-
-        taskExecuteVoid(() -> mSongSyncManager.syncMbsProWithDrive(TEST_FOLDER_ROOT_ID, saveLocationUri))
-                .addOnFailureListener(exception -> Log.e(TAG, "Sync failed", exception));
+        startSyncJob(false, result.getData());
     }
 
     private void handleStartBackgroundSync(int resultCode, Intent result)
@@ -184,24 +167,27 @@ public class MainActivity extends AppCompatActivity
             return;
         }
 
-        final Uri saveLocationUri = result.getData();
-        ProdAssert.notNull(saveLocationUri);
+        startSyncJob(true, result.getData());
+    }
+
+    private void startSyncJob(boolean isPeriodic, Uri mbsProDataDir)
+    {
+        ProdAssert.notNull(mbsProDataDir);
+
+        PersistableBundle persistableBundle = new PersistableBundle();
+        persistableBundle.putString(SongSyncJobService.MBS_PRO_DATA_DIR, mbsProDataDir.toString());
+        persistableBundle.putString(SongSyncJobService.DRIVE_FOLDER_ID, TEST_FOLDER_ROOT_ID);
 
         JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        JobInfo jobInfo = new JobInfo.Builder(JOB_ID_BG_SYNC, new ComponentName(this, SongSyncJobService.class))
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-                .setPeriodic(BG_SYNC_INTERVAL_MILLIS)
-                .build();
-        jobScheduler.schedule(jobInfo);
-    }
+        JobInfo.Builder jobInfoBuilder = new JobInfo.Builder(SongSyncJobService.JOB_ID, new ComponentName(this, SongSyncJobService.class))
+                .setExtras(persistableBundle)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);
 
-    private <T> Task<T> taskExecute(Callable<T> c)
-    {
-        return Tasks.call(mExecutor, FunctionWrapper.createTyped(c));
-    }
+        if (isPeriodic)
+        {
+            jobInfoBuilder.setPeriodic(BG_SYNC_INTERVAL_MILLIS);
+        }
 
-    private Task<Void> taskExecuteVoid(FunctionWrapper.VoidFunction f)
-    {
-        return Tasks.call(mExecutor, FunctionWrapper.createVoid(f));
+        jobScheduler.schedule(jobInfoBuilder.build());
     }
 }
